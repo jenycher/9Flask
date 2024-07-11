9f1243a97638732600f382cceb85b32d56c1bb65
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-11 18:10:18 +0300

    Update 9flask-2.py

--- 9flask-2.py
+++ 9flask-2.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<name>/")
+@app.route("/<password>/")
 def hello_world(name="незнакомец"):
     return f"Привет, {name}"
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     app.run()
