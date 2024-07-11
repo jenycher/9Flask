56d84b3588fcef2bfcc56ad79ee39f03613272b7
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-11 22:06:58 +0300

    add req

--- dz9-flask2.py
+++ dz9-flask2.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def filsms():
-    return render_template("index-b.html")
+def index():
+    return render_template("index.html")
 
 
-@app.route("/person/")
+@app.route("/blog/")
 
-def person():
-    return render_template("index-p.html")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contacts/")
+
+def contacts():
+    return render_template("contacts.html")
 
 if __name__ == "__main__":
     app.run()
