d2882e5150e6f10583eb4363125abb2e9f28eec9
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-11 20:38:00 +0300

    Update 9flask-2.py

--- 9flask-2.py
+++ 9flask-2.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<password>/")
-def hello_world(name="незнакомец"):
-    return f"Привет, {name}"
+def hello_world(password=None):
+    if password =="1234":
+        return f"Доступ разрешен"
+    else:
+        return f"Доступ запрещен"
 
 if __name__ == "__main__":
     app.run()
