513689703a1da69412559fd14cccf78fc1d51cd6
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-12 00:11:45 +0300

    summ

--- 9jinja-1.py
+++ 9jinja-1.py
@@ -6,14 +6,30 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def filsms():
-    return render_template("index-b.html")
 
+def filsms():
+    context={
+        "caption": "Фильмы про Гарри",
+        "user": "Нина",
+        "num": 1
+    }
+    return render_template("shablon.html", **context)
+
+
+@app.route("/shablon/")
+def films2():
+    context = {
+        "caption": "Гарри Поттер",
+        "link": "Перейти в кинотеатр"
+    }
+    return render_template("index-bb.html", **context)
 
 @app.route("/person/")
 
 def person():
     return render_template("index-p.html")
+
+
 
 if __name__ == "__main__":
     app.run()
