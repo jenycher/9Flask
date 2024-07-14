df576083f6c6aef018083c9b7b1fa679c8f131f2
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-14 19:23:23 +0300

    summ req

--- 9jinja-1.py
+++ 9jinja-1.py
@@ -1,35 +1,19 @@
-import flask
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
-
 @app.route("/")
 
-def filsms():
-    context={
-        "caption": "Фильмы про Гарри",
-        "user": "Нина",
-        "num": 1
+def films():
+    context = {
+        "poem": [
+            "Сижу за решёткой в темнице сырой.",
+            "Вскормленный в неволе орёл молодой,",
+            "Мой грустный товарищ, махая крылом,",
+            "Кровавую пищу клюёт под окном,",
+            "Клюёт, и бросает, и смотрит в окно,",
+            "Как будто со мною задумал одно."]
     }
     return render_template("shablon.html", **context)
-
-
-@app.route("/shablon/")
-def films2():
-    context = {
-        "caption": "Гарри Поттер",
-        "link": "Перейти в кинотеатр"
-    }
-    return render_template("index-bb.html", **context)
-
-@app.route("/person/")
-
-def person():
-    return render_template("index-p.html")
-
-
 
 if __name__ == "__main__":
     app.run()
