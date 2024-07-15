60368e0d0c045fd660d8d0c65907f0bef5ce607a
jenycher <168509289+jenycher@users.noreply.github.com>
2024-07-15 11:59:02 +0300

    ass

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ssite.db'
 
 from app import routes
--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Имя пользователя', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('New Password')
-    submit = SubmitField('Save Changes')
+    password = PasswordField('Новый пароль')
+    submit = SubmitField('Сохранить')
